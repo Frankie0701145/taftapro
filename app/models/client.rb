54ec51a6cd77e4ea75e2963805450d643ef25f63
Andrew Henry <andystudy4@gmail.com>
2018-06-12 11:47:13 +0300

    Add a client resource and validate email presence and format

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,7 @@
 class Client < ApplicationRecord
-	validates :name, presence: true
+	validates :name, :email, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+					format: { with: VALID_EMAIL_REGEX },
+					uniqueness: { case_sensitive: false }	
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@client = Client.new(name: "John Doe", email: "johndoe@gmail.com")
+	end
+
+  test "should be a valid client" do
+    assert @client.valid?
+  end  
+  
+  test "name should be present" do
+  	@client.name = " "
+    assert_not @client.valid?   
+  end  
+
+  test "email should be present" do
+  	@client.email = " "
+    assert_not @client.valid?   
+  end    
+
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+							first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
+			@client.email = valid_address
+			assert @client.valid?, "#{valid_address.inspect} should be valid"
+		end  
+	end
+
+	test "email validation should reject invalid addresses" do
+		invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+								foo@bar_baz.com foo@bar+baz.com]
+		invalid_addresses.each do |invalid_address|
+			@client.email = invalid_address
+			assert_not @client.valid?, "#{invalid_address.inspect} should be invalid"
+		end
+	end	
 end
