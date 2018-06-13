87d96b6796bd59741766631fcc254a9dbddff21e
Andrew Henry <andystudy4@gmail.com>
2018-06-14 01:42:57 +0300

    Show client name on navbar upon logging in, improve code style

--- app/controllers/client_sessions_controller.rb
+++ app/controllers/client_sessions_controller.rb
@@ -9,7 +9,7 @@ class ClientSessionsController < ApplicationController
 
     if client && client.authenticate(params[:client_session][:password])
       #loggin in the user
-      client_login client
+      client_login(client)
       redirect_to client
     else
       #render the login page again this time with a flash message
@@ -20,7 +20,7 @@ class ClientSessionsController < ApplicationController
 
   def destroy
     client_logout
-    render html:"<h1></logged_out</h1>"
+    redirect_to root_url
   end
 
 end
