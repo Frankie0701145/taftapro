c0687dde40dac62b27fb6b8f406c265321576863
Andrew Henry <andystudy4@gmail.com>
2018-06-14 02:25:22 +0300

    Flash relevant messages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,10 +4,20 @@ class StaticPagesController < ApplicationController
   end
 
   def search_service
-  	@professionals = Professional.near(params[:search_service_by_location_form][:location]) &&
-  					Professional.where(service: params[:search_service_by_location_form][:service])
+  	if !params[:search_service_by_location_form][:location].empty? && 
+  		!params[:search_service_by_location_form][:service].empty?
+  		@professionals = Professional.near(params[:search_service_by_location_form][:location]) &&
+  					Professional.where(service: params[:search_service_by_location_form][:service])  		
+  	elsif params[:search_service_by_location_form][:location].empty?
+  		flash.now[:info] = "There's no results for the location you have entered."
+  	elsif params[:search_service_by_location_form][:service].empty?
+  		flash.now[:info] = "There's no results for the service you have entered."
+  	else
+  		flash.now[:info] = "There's no results for your search criteria."
+    end
   end
 
   def about
   end
+
 end
