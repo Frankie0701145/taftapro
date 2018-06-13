9cbe336d09d68b087ed7419d6ba6a6122eecabcd
Francis <coulsorfrancois@gmail.com>
2018-06-13 16:17:51 +0300

    added the methods to generate digest token and method to save reset_digest token

--- app/models/client.rb
+++ app/models/client.rb
@@ -18,18 +18,39 @@
 #
 
 class Client < ApplicationRecord
+	attr_accessor :reset_token
+
 	before_save :downcase_email
 	validates :first_name, :last_name, :email, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
 					format: { with: VALID_EMAIL_REGEX },
 					uniqueness: { case_sensitive: false }
-	has_secure_password	
+	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
+
+	# Returns the hash digest of the given string.
+	def Client.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+																									BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+	# Returns a random token.
+	def Client.new_token
+			SecureRandom.urlsafe_base64
+	end
+
+	#Sets the password reset attributes and saves it to the client model
+	def create_reset_digest
+		self.reset_token = Client.new_token
+		update_attribute(:reset_digest, Client.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
 
 	private
 
 	def downcase_email
 		email.downcase!
-	end					
+	end
 end
