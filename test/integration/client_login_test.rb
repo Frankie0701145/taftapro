b164fb8bd7aed64fb2333480f6b576b386f05a54
Andrew Henry <andystudy4@gmail.com>
2018-06-13 11:34:47 +0300

    Improve test for login with invalid credentials

--- test/integration/client_login_test.rb
+++ test/integration/client_login_test.rb
@@ -7,15 +7,16 @@ class ClientLoginTest < ActionDispatch::IntegrationTest
     get client_login_path
     #confirm that the acquired view is the client login view
     assert_response :success
+    assert_template 'client_sessions/new'
     #post the wrong login credentials
     post client_login_path, params:{client_session:{email:"",password:""}}
-
     #confirm that the acquired view is the client login view when the credentials are wrong
-    assert_response :success
-
+    assert_template 'client_sessions/new'
     #confirm the the flash.message has a message
     assert_not flash.empty?
-
+    # Confirm that the flash used is flash.now and does not show in the next request
+    get root_path
+    assert flash.empty?
   end
 
 end
