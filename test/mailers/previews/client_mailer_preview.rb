b4518e03a32f97cf20daf1bd6ffb6b23dbfc4006
Andrew Henry <andystudy4@gmail.com>
2018-06-14 17:51:50 +0300

    Fix issue with mailer previews

--- test/mailers/previews/client_mailer_preview.rb
+++ test/mailers/previews/client_mailer_preview.rb
@@ -8,7 +8,9 @@ class ClientMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/client_mailer/password_reset
   def password_reset
-    ClientMailer.password_reset
+  	client = Client.first
+  	client.reset_token = Client.new_token
+    ClientMailer.password_reset(client)
   end
 
 end
