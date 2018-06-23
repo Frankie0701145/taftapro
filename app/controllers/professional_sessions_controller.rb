74ed3eeb0b8b52ad9af10b354d3486cd2a2f02e0
Francis <coulsorfrancois@gmail.com>
2018-06-23 11:44:52 +0300

    Added the signup with an invalid credential feature and the corresponding test for porfessional

--- app/controllers/professional_sessions_controller.rb
+++ app/controllers/professional_sessions_controller.rb
@@ -1,4 +1,18 @@
 class ProfessionalSessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    professional= Professional.find_by(email: params[:professional_session][:email].downcase)
+
+    if professional && professional.authenticate(params[:professional_session][:password])
+      #the pro login
+    else
+      #the pro does not login
+      flash.now[:danger] = "Invalid password/email"
+      render "new"
+    end
+
   end
 end
