95e8f1d66b67fe11bf808867df292fe1f95f1aba
Francis <coulsorfrancois@gmail.com>
2018-07-02 11:49:31 +0300

    Created the professional_mailer_preview of the professional reset password

--- test/mailers/previews/professional_mailer_preview.rb
+++ test/mailers/previews/professional_mailer_preview.rb
@@ -3,7 +3,10 @@ class ProfessionalMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/professional_mailer/password_reset
   def password_reset
-    ProfessionalMailer.password_reset
+    professional= Professional.first
+    professional.reset_token = Professional.new_token
+    ProfessionalMailer.password_reset(professional)
+
   end
 
 end
