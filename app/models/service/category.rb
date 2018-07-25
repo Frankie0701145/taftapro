5b2aee40d8ab6ad24ee7c4ddff8661e068ce5d4a
Andrew Henry <andystudy4@gmail.com>
2018-07-26 02:45:56 +0300

    Create class methods to create categories and questions WIP

--- app/models/service/category.rb
+++ app/models/service/category.rb
@@ -13,8 +13,19 @@ class Service::Category < ApplicationRecord
   has_many :questions
 
 
-  def self.home_improvement
-		home_improvement_category = Service::Category.create(name: "Home Improvement")    
-		question_one = home_improvement_category.questions.build(question: "Where do you need the service?")
+  def self.create_home_improvement_category
+		home_improvement_category = Service::Category.find_or_create_by(name: "Home Improvement")    
+		
+		question_one = 
+			Question.find_by(question: "Where do you need the service?", category_id: home_improvement_category.id) || home_improvement_category.questions.build(question: "Where do you need the service?")
+		question_two = home_improvement_category.questions.build(question: "Describe the nature of the service.")
+		question_three = home_improvement_category.questions.build(question: "Describe the nature of the service.")
+
+		home_improvement_category.save
+  end
+
+  def self.home_improvement_category
+  	home_improvement_category = Service::Category.where(name: "Home Improvement")    
+  	home_improvement_category[0]
   end
 end
