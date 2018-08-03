e9b74a6679171686ff542b9abfd8b20eb29b54f3
Andrew Henry <andystudy4@gmail.com>
2018-08-03 17:02:01 +0300

    Added type to questions

--- app/models/service/category.rb
+++ app/models/service/category.rb
@@ -31,23 +31,42 @@ class Service::Category < ApplicationRecord
 								"Laundry"]
 
   def self.create_home_improvement_category
+  	first_question = { question: "Describe the nature of the service.", type: "text_area" }
+  	second_question = { question: "Which additional service do you want if any?", type: "text_box" }
+  	third_question = { question: "When do you need this service done – calendar?", type: "calendar" }
+  	last_question = { question: "Where can we send your matches?", type: "email" }
+
   	@@home_improvement_services.each do |home_improvement_service|
 	    home_improvement_category = Service::Category.find_or_create_by(name: "Home Improvement", service: home_improvement_service)    
 	    
 	    if home_improvement_category
 	      question_one = 
 	        Question.find_by(
-	        	question: "Where do you need the service?", 
+	        	question: first_question[:question], 
 	        	category_id: home_improvement_category.id
 	        	) || 
-	        home_improvement_category.questions.build(question: "Where do you need the service?")
+	        home_improvement_category.questions.build(question: first_question[:question], type: first_question[:type])
 
 	      question_two = 
 	        Question.find_by(
-	        	question: "Describe the nature of the service.", 
+	        	question: second_question, 
 	        	category_id: home_improvement_category.id
 	        	) ||       	
-	      	home_improvement_category.questions.build(question: "Describe the nature of the service.")
+	      	home_improvement_category.questions.build(question: second_question[:question], type: second_question[:type])
+
+		  question_three = 
+	        Question.find_by(
+	        	question: third_question[:question], 
+	        	category_id: home_improvement_category.id
+	        	) ||       	
+	      	home_improvement_category.questions.build(question: third_question[:question], type: third_question[:type])
+
+	      question_last = 
+	        Question.find_by(
+	        	question: last_question[:question], 
+	        	category_id: home_improvement_category.id
+	        	) ||       	
+	      	home_improvement_category.questions.build(question: last_question[:question], type: last_question[:type])	      
 
 	      home_improvement_category.save
 	    end
