3a3282c6b780c9094bd146ea944120e0e8f480c2
Andrew Owino <andystudy4@gmail.com>
2018-09-22 20:44:49 +0300

    Update lat and lng in instances where it is nil

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,6 +1,6 @@
 class ProfessionalsController < ApplicationController
   before_action :logged_in_professional, only: [:edit, :update]
-  #before_action :logged_in_client, only:[:show]
+
   def new
     @professional = Professional.new()
   end
@@ -26,7 +26,13 @@ class ProfessionalsController < ApplicationController
 
   def show
     @professional = Professional.find(params[:id])
-    #@request = Request.new
+
+    # Geocoder is already hit when a pro creates an account
+    unless URI(request.referrer).path == "/professional/signup"
+      # To prevent QUERY_OVER_LIMIT errors don't hit geocoder otherwise. 
+      lookup_ip_location if @professional == current_professional
+    end
+
     @service= @professional.service
     @location=@professional.address
     @questions = Category.find_by(service: @service).questions unless professional_logged_in?
@@ -82,6 +88,29 @@ class ProfessionalsController < ApplicationController
 
   def quotation_params
     params.require(:quotation).permit(:quotation_document, :professional_id, :client_id, :request_id)
+  end
+
+  def lookup_ip_location
+    # Reduces the occurrence of a blank map
+    # If geocoder fails, a blank map will still occur
+    if @professional.address.present? && @professional.latitude.nil? && @professional.longitude.nil?  
+      location = @professional.city, @professional.country
+      ip_location = Geocoder.search(@professional.address)
+      
+      lat = ip_location[0].geometry["location"]["lat"]
+      lng = ip_location[0].geometry["location"]["lng"]
+
+      if ip_location.present?
+        lat = ip_location[0].geometry["location"]["lat"]
+        lng = ip_location[0].geometry["location"]["lng"]
+
+        if @professional.update_attributes(latitude: lat, longitude: lng) 
+          puts "Professional\'s Latitude and longitude updated successfully."
+        else
+          puts "Could not update pro\'s location on the map."
+        end
+      end
+    end
   end
 
 end
