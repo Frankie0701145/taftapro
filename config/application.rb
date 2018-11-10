373e084b9ade86ffc718d5a09831e4207e8f6703
Andrew Owino <andystudy4@gmail.com>
2018-11-10 22:03:55 +0300

    Use environment variables for google api keys

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,14 @@ require_relative "boot"
 require "rails/all"
 require "carrierwave"
 require "carrierwave/orm/activerecord"
+
+# preload tokens in application.yml to local ENV
+config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+config.merge! config.fetch(Rails.env, {})
+config.each do |key, value|
+  ENV[key] = value.to_s unless value.kind_of? Hash
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
