79d44288d06d1deb4f6b5c163717dc90c84a80d9
Francis <coulsorfrancois@gmail.com>
2018-11-30 13:13:07 +0300

    setup validation test for the quotation model

--- test/models/quotation_test.rb
+++ test/models/quotation_test.rb
@@ -25,4 +25,26 @@ class QuotationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @quotation = Quotation.new(:amount=>"1,000")
+  end
+  #the amount should not contain commas
+  test "amount should not contain commas" do
+      assert_not @quotation.valid?
+  end
+  #the amount should not contain words should only contain figures
+  test "amount should not contain only figures" do
+    quotation3 = Quotation.new(:amount=>"One hundred thousand")
+    assert_not quotation3.valid?
+  end
+  #the amount should not contain symbol should only contain figures
+  test "amount should not contain only currency symbols" do
+    quotation4 = Quotation.new(:amount=>"1000$")
+    assert_not quotation4.valid?
+  end
+  # the amount field should be not empty
+  test "amount should not be empty" do
+    quotation2 = Quotation.new(:amount=>'')
+    assert_not quotation2.valid?
+  end
 end
