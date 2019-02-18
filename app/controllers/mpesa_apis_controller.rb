9bfd3aad00702b279ac8ad4fb62c7c16159bfc0f
Francis <coulsorfrancois@gmail.com>
2019-02-18 17:42:02 +0300

    removed the mpesa word in the callback function and also retrieved the data directly from the json

--- app/controllers/mpesa_apis_controller.rb
+++ app/controllers/mpesa_apis_controller.rb
@@ -5,13 +5,13 @@ class MpesaApisController < ApplicationController
   #endpoint to validate the mpesa transaction
   def validation_callback
     #first check to see if there is a project with that project id which is the BillRefNumber
-    project = Project.find_by(id: params[:mpesa_api][:BillRefNumber])
+    project = Project.find_by(id: params[:BillRefNumber])
     #if there is a project with that project id(BillRefNumber) accept the payement
     if project
       # found the project
-      payment = Payment.new(trans_id: params[:mpesa_api][:TransID], client_id: project.client_id,
+      payment = Payment.new(trans_id: params[:TransID], client_id: project.client_id,
         project_id: project.id, professional_id: project.professional_id, payment_type: "mpesa",
-        amount: params[:mpesa_api][:TransAmount], trans_time: params[:mpesa_api][:TransTime], msisdn:params[:mpesa_api][:MSISDN],
+        amount: params[:TransAmount], trans_time: params[:TransTime], msisdn:params[:MSISDN],
         mpesa_status: "pending", status: Payment.status_pending
       )
       if payment.save
@@ -27,10 +27,10 @@ class MpesaApisController < ApplicationController
 
   #the confirmation_callback handler
   def confirmation_callback
-    payment = Payment.where(trans_id: params[:mpesa_api][:TransID], project_id: params[:mpesa_api][:BillRefNumber]).first
+    payment = Payment.where(trans_id: params[:TransID], project_id: params[:BillRefNumber]).first
     if payment
       payment.update_attributes(status:  Payment.status_completed, mpesa_status: "completed")
-      project = Project.find_by(id: params[:mpesa_api][:BillRefNumber])
+      project = Project.find_by(id: params[:BillRefNumber])
       if project
         project_debit_balance = project.debit_balance - payment.amount
         project_paid = project.paid + payment.amount
