4a2d3111e39d808a31ed51d92b67407c33e8c41d
Francis <coulsorfrancois@gmail.com>
2019-08-03 16:07:07 +0300

    Creates mpesa_apicontroller tests that confirms that if the access_token provided is wrong then the response back is bad request

--- test/controllers/mpesa_apis_controller_test.rb
+++ test/controllers/mpesa_apis_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class MpesaApisControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+    #confirmation endpoint
+    # test "should get the ok response from the confirmation url with correct password" do
+    #   accesstoken_query_string = {
+    #     access_token: Rails.application.credentials.MPESA_SECRET
+    #   }
+    #   post mpesa_confirmation_callback_path(accesstoken_query_string)
+    #   assert_response :success
+    # end
+
+    test "should get bad request from the confirmation url with wrong password" do
+      accesstoken_query_string = {
+        access_token: "uiujigg"
+      }
+      post mpesa_confirmation_callback_path(accesstoken_query_string)
+      assert_response :bad_request
+    end
+
+    #validation endpoint
+    # test "should get the ok response from the validation url with correct password" do
+    #   accesstoken_query_string = {
+    #     access_token: Rails.application.credentials.MPESA_SECRET
+    #   }
+    #   post mpesa_validation_callback_path(accesstoken_query_string)
+    #   assert_response :success
+    # end
+
+    test "should get bad request from the validation url with wrong password" do
+      accesstoken_query_string = {
+        access_token: "uiujigg"
+      }
+      post mpesa_validation_callback_path(accesstoken_query_string)
+      assert_response :bad_request
+    end
+
+
+
 end
